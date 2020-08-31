194acd71d749b1dc26a878fe0df5940411f8f211
Marcin Balut <marcinbalutpoland@gmail.com>
2020-08-31 12:27:45 +0200

    JWT Authentication

--- DatingApp.API/Controllers/ValueController.cs
+++ DatingApp.API/Controllers/ValueController.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValueController : ControllerBase
@@ -26,6 +28,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         } 
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
